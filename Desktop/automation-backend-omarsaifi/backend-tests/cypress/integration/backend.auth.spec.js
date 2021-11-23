fa719f194e2000f8de3b48192e7653217a35d140
Omar Saifi <omar.saifi@yh.nackademin.se>
2021-11-23 13:50:23 +0100

    Created a first test case

--- Desktop/automation-backend-omarsaifi/backend-tests/cypress/integration/backend.auth.spec.js
+++ Desktop/automation-backend-omarsaifi/backend-tests/cypress/integration/backend.auth.spec.js
@@ -1,1 +1,30 @@
-/// <reference types="cypress" />
+
+describe('Testing automation', function(){
+
+    it.only('Test Case 1 - Create a new client', function(){
+        cy.authenticateSession().then((response =>{
+            const payload = {
+                "name":"Omar Saifi",
+                "email":"omaralisaifi@hotmail.com",
+                "telephone":"0732365202"}
+            
+            // Post request to create a client 
+            cy.request({
+                method: "POST",
+                url: 'http://localhost:3000/api/client/new',
+                headers:{
+                    'X-User-Auth': JSON.stringify(Cypress.env().loginToken),
+                    'Content-Type': 'application/json'
+                },
+                body:payload
+            }).then((response =>{
+                cy.log(JSON.stringify(response))
+                const responseAsString = JSON.stringify(response)
+                expect(responseAsString).to.have.string(payload.name)
+  
+            }))
+        }))
+    })
+
+
+})
